03791f90bd1eaa9c2f1091723c682b53318b1fca
mats <mats.henricson@gmail.com>
2019-04-10 16:45:16 +0200

    Merged two tests that belonged together, and cleaned up things a bit.

---
+++ src/test/java/com/eternitywall/http/TestRequestResponse.java
@@ -0,0 +1,27 @@
+package com.eternitywall.http;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestRequestResponse {
+
+    @Test
+    public void testHttp() throws Exception {
+        Request request = new Request(new URL("http://httpbin.org/status/418"));
+        Response response = request.call();
+        assertEquals(418, response.getStatus().intValue());
+    }
+
+    @Test
+    public void testHttps() throws Exception {
+        Request request = new Request(new URL("https://finney.calendar.eternitywall.com"));
+        Response response = request.call();
+        assertTrue(response.isOk());
+        assertNotNull(response.getString());
+    }
+}
