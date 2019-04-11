7b6df0e9901b7092efd6c185b02aa1082678e4a1
mats <mats.henricson@gmail.com>
2019-04-11 15:20:57 +0200

    Some minor changes to Ops tests.

--- src/test/java/com/eternitywall/ots/op/TestOp.java
+++ src/test/java/com/eternitywall/ots/op/TestOp.java
@@ -11,37 +11,39 @@ public class TestOp {
 
     @Test
     public void testAppendOperation() {
-        assertArrayEquals(new OpAppend(toBytes("suffix", "UTF-8")).call(toBytes("msg", "UTF-8")),
-                          toBytes("msgsuffix", "UTF-8"));
+        assertArrayEquals(toBytes("msg" + "suffix", "UTF-8"),
+                          new OpAppend(toBytes("suffix", "UTF-8")).call(toBytes("msg", "UTF-8")));
     }
 
     @Test
     public void testPrependOperation() {
-        assertArrayEquals(new OpPrepend(toBytes("prefix", "UTF-8")).call(toBytes("msg", "UTF-8")),
-                          toBytes("prefixmsg", "UTF-8"));
+        assertArrayEquals(toBytes("prefix" + "msg", "UTF-8"),
+                          new OpPrepend(toBytes("prefix", "UTF-8")).call(toBytes("msg", "UTF-8")));
     }
 
     @Test
     public void testSha256Operation() {
         OpSHA256 opSHA256 = new OpSHA256();
-        byte[] msg = {};
-        assertArrayEquals(opSHA256.call(msg),
-                          DatatypeConverter.parseHexBinary("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"));
+        byte[] emptyMsg = {};
+        assertArrayEquals(DatatypeConverter.parseHexBinary("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
+                          opSHA256.call(emptyMsg));
     }
 
     @Test
     public void testRIPEMD160Operation() {
         OpRIPEMD160 opRIPEMD160 = new OpRIPEMD160();
-        byte[] msg = {};
-        assertArrayEquals(opRIPEMD160.call(msg),
-                          DatatypeConverter.parseHexBinary("9c1185a5c5e9fc54612808977ee8f548b2258d31"));
+        byte[] emptyMsg = {};
+        assertArrayEquals(DatatypeConverter.parseHexBinary("9c1185a5c5e9fc54612808977ee8f548b2258d31"),
+                          opRIPEMD160.call(emptyMsg));
     }
 
     @Test
     public void testOperationEquality() {
         assertEquals(new OpAppend(toBytes("foo", "UTF-8")), new OpAppend(toBytes("foo", "UTF-8")));
+        assertEquals(new OpPrepend(toBytes("foo", "UTF-8")), new OpPrepend(toBytes("foo", "UTF-8")));
         assertNotEquals(new OpAppend(toBytes("foo", "UTF-8")), new OpAppend(toBytes("bar", "UTF-8")));
         assertNotEquals(new OpAppend(toBytes("foo", "UTF-8")), new OpPrepend(toBytes("foo", "UTF-8")));
+        assertNotEquals(new OpPrepend(toBytes("foo", "UTF-8")), new OpAppend(toBytes("foo", "UTF-8")));
     }
 
     @Test
--- src/test/java/com/eternitywall/ots/op/TestOps.java
+++ src/test/java/com/eternitywall/ots/op/TestOps.java
@@ -13,43 +13,48 @@ public class TestOps {
     @Test
     public void testKeccak() {
         byte[] result = new OpKECCAK256().call(Utils.hexToBytes(""));
-        assertEquals("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", Utils.bytesToHex(result).toLowerCase());
+        assertEquals("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+                     Utils.bytesToHex(result).toLowerCase());
 
         byte[] result2 = new OpKECCAK256().call(Utils.hexToBytes("80"));
-        assertEquals("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421", Utils.bytesToHex(result2).toLowerCase());
+        assertEquals("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                     Utils.bytesToHex(result2).toLowerCase());
     }
 
     @Test
     public void testSha1() {
         byte[] result = new OpSHA1().call(Utils.hexToBytes("0a"));
-        assertEquals("adc83b19e793491b1c6ea0fd8b46cd9f32e592fc", Utils.bytesToHex(result).toLowerCase());
+        assertEquals("adc83b19e793491b1c6ea0fd8b46cd9f32e592fc",
+                     Utils.bytesToHex(result).toLowerCase());
     }
 
     @Test
     public void testSha256() {
         byte[] result = new OpSHA256().call(Utils.hexToBytes(""));
-        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", Utils.bytesToHex(result).toLowerCase());
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                     Utils.bytesToHex(result).toLowerCase());
     }
 
     @Test
     public void testRipemd() {
         byte[] result = new OpRIPEMD160().call(Utils.hexToBytes(""));
-        assertEquals("9c1185a5c5e9fc54612808977ee8f548b2258d31", Utils.bytesToHex(result).toLowerCase());
+        assertEquals("9c1185a5c5e9fc54612808977ee8f548b2258d31",
+                     Utils.bytesToHex(result).toLowerCase());
     }
 
     @Test
     public void testAppend() {
-        byte[] foos = Utils.hexToBytes("00");
-        byte[] bars = Utils.hexToBytes("11");
-        byte[] call = new OpAppend(foos).call(bars);
+        byte[] zeroes = Utils.hexToBytes("00");
+        byte[] ones = Utils.hexToBytes("11");
+        byte[] call = new OpAppend(zeroes).call(ones);
         assertEquals("1100", Utils.bytesToHex(call).toLowerCase());
     }
 
     @Test
     public void testPrepend() {
-        byte[] foos = Utils.hexToBytes("00");
-        byte[] bars = Utils.hexToBytes("11");
-        byte[] call = new OpPrepend(foos).call(bars);
+        byte[] zeroes = Utils.hexToBytes("00");
+        byte[] ones = Utils.hexToBytes("11");
+        byte[] call = new OpPrepend(zeroes).call(ones);
         assertEquals("0011", Utils.bytesToHex(call).toLowerCase());
     }
 
